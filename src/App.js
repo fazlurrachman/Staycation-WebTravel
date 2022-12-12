108fb9faf4dd66bb1297c483514f7f461141cc26
fazlurrachman <alul6628@gmail.com>
2022-12-12 14:56:27 +0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
+
+import 'assets/scss/style.scss';
+import LandingPage from 'pages/LandingPage';
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Route path="/" component={LandingPage}></Route>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/LandingPage.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+
+import Header from "parts/Header";
+import Hero from "parts/Hero";
+import MostPicked from "parts/MostPicked";
+import Categories from "parts/Categories";
+import Testimony from "parts/Testimony";
+import Footer from "parts/Footer";
+
+import landingPage from "json/landingPage.json";
+
+export default class LandingPage extends Component {
+  constructor(props) {
+    super(props);
+    this.refMostPicked = React.createRef();
+  }
+  render() {
+    return (
+      <>
+        <Header {...this.props}></Header>
+        <Hero refMostPicked={this.refMostPicked} data={landingPage.hero} />
+        <MostPicked
+          refMostPicked={this.refMostPicked}
+          data={landingPage.mostPicked}
+        />
+        <Categories data={landingPage.categories} />
+        <Testimony data={landingPage.testimonial} />
+        <Footer />
+      </>
+    );
+  }
+}
