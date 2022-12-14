69891b4ff667c41657138f42c13d831ebd5bdf18
fazlurrachman <alul6628@gmail.com>
2022-12-14 21:47:07 +0700

    adding react-awesome-reveal and bit transition each section

--- src/parts/Categories.js
+++ src/parts/Categories.js
@@ -1,42 +1,61 @@
-import React from 'react';
+import React from "react";
+import { Fade } from "react-awesome-reveal";
 
-import Button from 'elements/Button';
+import Button from "elements/Button";
 
 export default function Categories({ data }) {
   return data.map((category, index1) => {
     return (
       <section className="container" key={`category-${index1}`}>
-        <h4 className="mb-3 font-weight-medium">{category.name}</h4>
-        <div className="container-grid">
-          {category.items.length === 0 ? (
-            <div className="row">
-              <div className="col-auto align-items-center">There is no property at this category</div>
-            </div>
-          ) : (
-            category.items.map((item, index2) => {
-              return (
-                <div className="item column-3 row-1" key={`category-${index1}-item-${index2}`}>
-                  <div className="card">
-                    {item.isPopular && (
-                      <div className="tag">
-                        Popular <span className="font-weight-light">Choice</span>
-                      </div>
-                    )}
-                    <figure className="img-wrapper" style={{ height: 180 }}>
-                      <img src={item.imageUrl} alt={item.name} className="img-cover" />
-                    </figure>
-                    <Button type="link" href={`/properties/${item._id}`} className="stretched-link d-block text-gray-800">
-                      <h5 className="h4">{item.name}</h5>
-                    </Button>
-                    <span className="text-gray-500">
-                      {item.city}, {item.country}
-                    </span>
-                  </div>
+        <Fade direction="up">
+          <h4 className="mb-3 font-weight-medium">{category.name}</h4>
+          <div className="container-grid">
+            {category.items.length === 0 ? (
+              <div className="row">
+                <div className="col-auto align-items-center">
+                  There is no property at this category
                 </div>
-              );
-            })
-          )}
-        </div>
+              </div>
+            ) : (
+              category.items.map((item, index2) => {
+                return (
+                  <div
+                    className="item column-3 row-1"
+                    key={`category-${index1}-item-${index2}`}
+                  >
+                    <Fade direction="up" delay={300 * index2}>
+                      <div className="card">
+                        {item.isPopular && (
+                          <div className="tag">
+                            Popular{" "}
+                            <span className="font-weight-light">Choice</span>
+                          </div>
+                        )}
+                        <figure className="img-wrapper" style={{ height: 180 }}>
+                          <img
+                            src={item.imageUrl}
+                            alt={item.name}
+                            className="img-cover"
+                          />
+                        </figure>
+                        <Button
+                          type="link"
+                          href={`/properties/${item._id}`}
+                          className="stretched-link d-block text-gray-800"
+                        >
+                          <h5 className="h4">{item.name}</h5>
+                        </Button>
+                        <span className="text-gray-500">
+                          {item.city}, {item.country}
+                        </span>
+                      </div>
+                    </Fade>
+                  </div>
+                );
+              })
+            )}
+          </div>
+        </Fade>
       </section>
     );
   });
--- src/parts/MostPicked.js
+++ src/parts/MostPicked.js
@@ -1,35 +1,52 @@
-import React from 'react';
-import Button from 'elements/Button';
+import React from "react";
+import { Fade } from "react-awesome-reveal";
+
+import Button from "elements/Button";
 
 export default function MostPicked(props) {
   return (
     <section className="container" ref={props.refMostPicked}>
-      <h4 className="mb-3">Most Picked</h4>
-      <div className="container-grid">
-        {props.data.map((item, index) => {
-          return (
-            <div key={`mostpicked-${index}`} className={`item column-4${index === 0 ? ' row-2' : ' row-1'}`}>
-              <div className="card card-featured">
-                <div className="tag">
-                  ${item.price}
-                  <span className="font-weight-light">per {item.unit}</span>
+      <Fade direction="up">
+        <h4 className="mb-3">Most Picked</h4>
+        <div className="container-grid">
+          {props.data.map((item, index) => {
+            return (
+              <div
+                key={`mostpicked-${index}`}
+                className={`item column-4${index === 0 ? " row-2" : " row-1"}`}
+              >
+                {/* <Fade direction="up" delay={300 * index}> */}
+                <div className="card card-featured">
+                  <div className="tag">
+                    ${item.price}
+                    <span className="font-weight-light">per {item.unit}</span>
+                  </div>
+                  <figure className="img-wrapper">
+                    <img
+                      src={item.imageUrl}
+                      alt={item.name}
+                      className="img-cover"
+                    />
+                  </figure>
+                  <div className="meta-wrapper">
+                    <Button
+                      type="link"
+                      className="streched-link d-block text-white"
+                      href={`/properties/${item._id}`}
+                    >
+                      <h5>{item.name}</h5>
+                    </Button>
+                    <span>
+                      {item.city}, {item.country}
+                    </span>
+                  </div>
                 </div>
-                <figure className="img-wrapper">
-                  <img src={item.imageUrl} alt={item.name} className="img-cover" />
-                </figure>
-                <div className="meta-wrapper">
-                  <Button type="link" className="streched-link d-block text-white" href={`/properties/${item._id}`}>
-                    <h5>{item.name}</h5>
-                  </Button>
-                  <span>
-                    {item.city}, {item.country}
-                  </span>
-                </div>
+                {/* </Fade> */}
               </div>
-            </div>
-          );
-        })}
-      </div>
+            );
+          })}
+        </div>
+      </Fade>
     </section>
   );
 }
